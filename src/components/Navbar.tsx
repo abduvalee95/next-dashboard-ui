0ab3541126672c1924ec4a07e73663f427c1c3fb
abduvalee95 <abduvalee95@gmail.com>
2025-03-03 11:13:03 +0900

    fix: Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 	return (
 		<div className='flex item-center justify-between p-4'>
 			{/* Search Bar */}
-			<div className='hidden md:flex items-center bg-white rounded-full ring-[1.5px] ring-gray-200 px-2'>
+			<div className='hidden md:flex items-center bg-white rounded-full ring-[1.5px] ring-gray-200 px-2'>˝
 				<Image src='/search.png' alt='search' width={14} height={14} />
 				<input
 					type='text'
